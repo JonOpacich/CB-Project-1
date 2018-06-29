c2c52c5f527b74f46bdbb30b17859349cff2ea10
Amanda Malmin <amandamalmin@gmail.com>
2018-06-29 10:56:55 -0500

    basic address input boxes created

---
+++ assets/javascript/schedule.js
@@ -0,0 +1,13 @@
+//soooooooo we present a form where people fill out their availability by checking each box they're available each day of the week (travel times not included)
+//for what kind of time period? Generic? Go week by week? Option for each
+//does each person have true/false for each time slot??
+
+//this availability is stored on firebase with their login 
+
+
+//can integrate reminders through google calendar. 
+
+//more helpful to practice building myself or using google's features? I think build.
+
+
+//make it so need to answer address to un-hide the schedule form
