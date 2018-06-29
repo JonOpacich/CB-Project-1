4afa7284f7590150e73eb2e91c3812c3327344c9
Amanda Malmin <amandamalmin@gmail.com>
2018-06-29 11:58:26 -0500

    basic schedule form outlined

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -11,3 +11,74 @@
 
 
 //make it so need to answer address to un-hide the schedule form
+
+
+$(document).ready(function () {
+
+    //an array with this weeks dates
+
+    let tableDay = "";
+    for (let weekdayIndex = 7; weekdayIndex > 0; weekdayIndex--) {
+        tableDay = moment().day(0 - weekdayIndex).format("ddd Do");
+        $("#table-header").append(
+            `<th scope="col">${tableDay}</th>`
+        )
+    }
+
+    //need to convert from military time (if statement is attempt of that)
+    for (let hourIndex = 7; hourIndex < 22; hourIndex++) {
+        // if (hourIndex < 12) {
+        let firstTimeSlotNum = hourIndex;
+        let secondTimeSlotNum = hourIndex + 1;
+        let timeSlot = `${firstTimeSlotNum}-${secondTimeSlotNum}`
+        //could do a loop here instead so not so repetitive
+        $("#table-body").append(
+            `<tr>
+                <th scope="row">${timeSlot}</th>
+                    <td>
+                        <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="" value="false" aria-label="...">
+                        </div>
+                    </td>
+                    <td>
+                        <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="" value="false" aria-label="...">
+                        </div>
+                    </td>
+                    <td>
+                        <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="" value="false" aria-label="...">
+                        </div>
+                    </td>
+                    <td>
+                        <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="" value="false" aria-label="...">
+                        </div>
+                    </td>
+                    <td>
+                        <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="" value="false" aria-label="...">
+                        </div>
+                    </td>
+                    <td>
+                        <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="" value="false" aria-label="...">
+                        </div>
+                    </td>
+                    <td>
+                        <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="" value="false" aria-label="...">
+                        </div>
+                    </td>
+                </tr>`
+                
+        )
+        // } else {
+        //     let firstTimeSlotNum = hourIndex - 12;
+        //     let secondTimeSlotNum = hourIndex -12 + 1;
+        //     let timeSlot = `${firstTimeSlotNum}-${secondTimeSlotNum}`
+        //     console.log(timeSlot)
+        // }
+    }
+
+})
