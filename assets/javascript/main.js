a4be48a35df1c38142a7aea60406ac0718f6f92a
jon Opacich <jonopacich@gmail.com>
2018-06-30 15:33:02 -0500

    map, oauth added

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,18 @@
-$(document).ready(function () {
+
+function initMap() {
+    //map options
+    var options={
+        zoom:11,
+        center:{lat:45,lng:-93.26},
+    }
+
+    //new map
+    var map=new
+    google.maps.Map(document.getElementById('map'), options);
+}
+$(document).ready(function(){
+
+
 
 
 })
