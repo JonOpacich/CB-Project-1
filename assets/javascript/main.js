980559d52d277b0028e9521b414930a69acbad19
Amanda Malmin <amandamalmin@gmail.com>
2018-07-02 14:20:25 -0500

    geocoded and randomized addresses

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,71 +1,85 @@
+// Initialize team Firebase
+var config = {
+    apiKey: "AIzaSyAGrun6JkFUDgSTJRFqDyPxzT0ymkDdPwI",
+    authDomain: "session-a4950.firebaseapp.com",
+    databaseURL: "https://session-a4950.firebaseio.com",
+    projectId: "session-a4950",
+    storageBucket: "session-a4950.appspot.com",
+    messagingSenderId: "298286458371"
+};
+firebase.initializeApp(config);
+let database = firebase.database();
 
-function initMap() {
-    //map options
-    var options = {
-        zoom: 11,
-        center: { lat: 45, lng: -93.26 },
+    function initMap() {
+        
+        var options = {
+            zoom: 11,
+            center: { lat: 45, lng: -93.26 },
+        }
+
+        //new map
+        var map = new
+            google.maps.Map(document.getElementById('map'), options);
     }
 
-    //new map
-    var map = new
-        google.maps.Map(document.getElementById('map'), options);
-}
+
+
 
 $(document).ready(function () {
 
-    //onclick handler for 'add-session' form
-    var eventButton = $('#add-event');
-    var closeForm = () => {
-        eventButton.removeClass('event-animate');
-        //code used as a 'hack' to allow animation to run multiple times
-        eventButton.offsetWidth = eventButton.offsetWidth;
-        $('#map').css('grid-column', '2/5')
-        $('#add-button').css('right', '4.5%');
-        eventButton.addClass("event-hide");
-    }
-
-    $('#add-button').on('click', () => {
-        if (eventButton.hasClass('event-animate')) {
-            closeForm();
-        }
-        else {
-            eventButton.removeClass('event-hide');
-            $('#map').css('grid-column', '2/4');
-            $('#add-button').css('right', '32%');
+        //onclick handler for 'add-session' form
+        var eventButton = $('#add-event');
+        var closeForm = () => {
+            eventButton.removeClass('event-animate');
+            //code used as a 'hack' to allow animation to run multiple times
             eventButton.offsetWidth = eventButton.offsetWidth;
-            eventButton.addClass('event-animate');
+            $('#map').css('grid-column', '2/5')
+            $('#add-button').css('right', '4.5%');
+            eventButton.addClass("event-hide");
         }
-    });
 
-    $('.close').on('click', function () {
-        closeForm();
+        $('#add-button').on('click', () => {
+            if (eventButton.hasClass('event-animate')) {
+                closeForm();
+            }
+            else {
+                eventButton.removeClass('event-hide');
+                $('#map').css('grid-column', '2/4');
+                $('#add-button').css('right', '32%');
+                eventButton.offsetWidth = eventButton.offsetWidth;
+                eventButton.addClass('event-animate');
+            }
+        });
+
+        $('.close').on('click', function () {
+            closeForm();
+        })
+
+
+        //onclick handler for chat
+        var chatButton = $('#chat-icon');
+        var chatDiv = $('#chat-div');
+
+        chatButton.on('click', function () {
+            if (chatDiv.hasClass('chat-animate')) {
+                chatDiv.removeClass('chat-animate');
+                chatDiv.offsetWidth = chatDiv.offsetWidth;
+                chatDiv.addClass("chat-hide");
+            }
+            else {
+                chatDiv.removeClass('chat-hide');
+                chatDiv.offsetWidth = chatDiv.offsetWidth;
+                chatDiv.addClass('chat-animate');
+
+            }
+        })
+
+
+
+
+
+
+
     })
-
-
-    //onclick handler for chat
-    var chatButton = $('#chat-icon');
-    var chatDiv=$('#chat-div');
-
-    chatButton.on('click', function () {
-        if (chatDiv.hasClass('chat-animate')) {
-            chatDiv.removeClass('chat-animate');
-            chatDiv.offsetWidth = chatDiv.offsetWidth;
-            chatDiv.addClass("chat-hide");
-        }
-        else {
-            chatDiv.removeClass('chat-hide');
-            chatDiv.offsetWidth = chatDiv.offsetWidth;
-            chatDiv.addClass('chat-animate');
-            
-        }
-    })
-
-
-
-
-
-
-
-})
 
 
--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -1,8 +1,8 @@
+//make this giant if statment so may just skip the whole thing if already entered info
 //this availability is stored on firebase with their login 
-
 //schedule needs to update for every 2 weeks?
 
-// Initialize Firebase
+// Initialize team Firebase
 var config = {
     apiKey: "AIzaSyAGrun6JkFUDgSTJRFqDyPxzT0ymkDdPwI",
     authDomain: "session-a4950.firebaseapp.com",
@@ -19,6 +19,7 @@ let string = "";
 let hourPosition = 7;
 let userScheduleArray = [];
 
+//creates horizontal checkbox based on how many days of the week displayed, set specific id for each one
 function createCheckboxes() {
     let scheduleLength = 7;
     let weekdayPosition = 0;
@@ -31,7 +32,7 @@ function createCheckboxes() {
 
 $(document).ready(function () {
 
-    
+    // displays days of week starting with Sunday
     let tableDay = "";
     for (let weekdayIndex = 7; weekdayIndex > 0; weekdayIndex--) {
         tableDay = moment().day(0 - weekdayIndex).format("dddd");
@@ -58,7 +59,7 @@ $(document).ready(function () {
     // *add disable class if todays date is after the date list on the top of the form somehow.....
     */
 
-    //* need to convert from military time
+    //displays study times 7am-10pm
     for (let hourIndex = 7; hourIndex < 22; hourIndex++) {
         let firstTimeSlotNum = moment(hourIndex, "k").format("h");
         let secondTimeSlotNum = moment(hourIndex + 1, "k").format("h a");
@@ -73,7 +74,7 @@ $(document).ready(function () {
     }
 
 
-    //change the value from true/false when checkbox checked/unchecked
+    //change the value of each check box from true/false when checked/unchecked
     $(document).on("click", ".form-check-input", function () {
         if ($(this).attr("value") == "false") {
             $(this).attr("value", "true")
@@ -83,20 +84,37 @@ $(document).ready(function () {
     });
 
 
-    //submit address to Firebase/hide from view
+    //Once click address submit, converts to geocode, round specifics off address, send to Firebase, switch view to schedule form
     $("#address-submit-btn").on("click", function (event) {
         event.preventDefault();
         let address = $("#street-address").val().trim();
+        let name = $("#name").val().trim();
         let city = $("#city").val().trim();
         let state = $("#state").val().trim();
         let zip = $("#zip").val().trim();
+        let lat = "";
+        let lng = "";
 
-        database.ref("User Address").push({
-            address: address,
-            city: city,
-            state: state,
-            zip: zip
+        let queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + ", " + city + ", " + state + zip + "&key=AIzaSyCk5A3HKmPbJQoTtpMg_RbAsy6NBEJS-Gk";
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": queryURL,
+            "method": "GET",
+            }
+          
+          $.ajax(settings).done(function (response) {
+            lat = response.results[0].geometry.location.lat.toFixed(3);
+            lng = response.results[0].geometry.location.lng.toFixed(3);
+
+            database.ref("User Address").push({
+                name: name,
+                lat: lat,
+                lng: lng
+            });
         });
+        
 
         $("#addressForm").addClass("d-none");
         $("#schedule-form").removeClass("d-none");
