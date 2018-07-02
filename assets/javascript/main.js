c0eb70ce205ee72e0903cbcd2a693dd444ec6f3e
jon Opacich <jonopacich@gmail.com>
2018-07-01 20:39:16 -0500

    pre-footer commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -11,33 +11,61 @@ function initMap() {
         google.maps.Map(document.getElementById('map'), options);
 }
 
-
 $(document).ready(function () {
 
+    //onclick handler for 'add-session' form
     var eventButton = $('#add-event');
-    var closeForm=()=>{
-        eventButton.removeClass('animate');
+    var closeForm = () => {
+        eventButton.removeClass('event-animate');
+        //code used as a 'hack' to allow animation to run multiple times
         eventButton.offsetWidth = eventButton.offsetWidth;
         $('#map').css('grid-column', '2/5')
         $('#add-button').css('right', '4.5%');
-        eventButton.addClass("hide");
+        eventButton.addClass("event-hide");
     }
-    
+
     $('#add-button').on('click', () => {
-        if (eventButton.hasClass('animate')) {
+        if (eventButton.hasClass('event-animate')) {
             closeForm();
         }
         else {
-            eventButton.removeClass('hide');
+            eventButton.removeClass('event-hide');
             $('#map').css('grid-column', '2/4');
             $('#add-button').css('right', '32%');
             eventButton.offsetWidth = eventButton.offsetWidth;
-            eventButton.addClass('animate');
+            eventButton.addClass('event-animate');
         }
     });
-    $('.close').on('click',function(){
+
+    $('.close').on('click', function () {
         closeForm();
     })
+
+
+    //onclick handler for chat
+    var chatButton = $('#chat-icon');
+    var chatDiv=$('#chat-div');
+
+    chatButton.on('click', function () {
+        if (chatDiv.hasClass('chat-animate')) {
+            chatDiv.removeClass('chat-animate');
+            chatDiv.offsetWidth = chatDiv.offsetWidth;
+            chatDiv.addClass("chat-hide");
+        }
+        else {
+            chatDiv.removeClass('chat-hide');
+            chatDiv.offsetWidth = chatDiv.offsetWidth;
+            chatDiv.addClass('chat-animate');
+            
+        }
+    })
+
+
+
+
+
+
+
 })
- 
+
 
