b0188aae6246b3dfc1b39dae43a2a047e0a00214
Amanda Malmin <amandamalmin@gmail.com>
2018-07-02 10:22:37 -0500

    changed to generic 1 week schedule, info submitting to Firebase

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -1,14 +1,11 @@
 //soooooooo we present a form where people fill out their availability by checking each box they're available each day of the week (travel times not included)
 //for what kind of time period? Generic? Go week by week? Option for each
-//does each person have true/false for each time slot??
 
 //this availability is stored on firebase with their login 
 
-//can integrate reminders through google calendar. 
-
-
 //make it so need to answer address to un-hide the schedule form
 //schedule needs to update for every 2 weeks?
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyDpx_ce6nfAL5lmBW6m4j6SSCKPktPxmXM",
@@ -28,7 +25,7 @@ let hourPosition = 7;
 let userScheduleArray = [];
 
 function createCheckboxes() {
-    let scheduleLength = 14;
+    let scheduleLength = 7;
     let weekdayPosition = 0;
     for (let checkboxIndex = 0; checkboxIndex < scheduleLength; checkboxIndex++) {
         string = string + `<td><div class="form-check"><input class="form-check-input position-static" type="checkbox" id="day-${weekdayPosition}-hr-${hourPosition}" value=false aria-label="..."></div></td>`
@@ -39,14 +36,22 @@ function createCheckboxes() {
 
 $(document).ready(function () {
 
-    // *add disable class if todays date is after the date list on the top of the form somehow.....
+    
     let tableDay = "";
-    //starts at the top of this week
     for (let weekdayIndex = 7; weekdayIndex > 0; weekdayIndex--) {
+        tableDay = moment().day(0 - weekdayIndex).format("dddd");
+        $("#table-header").append(
+            `<th scope="col">${tableDay}</th>`
+        );
+    }
+
+    //The section enters days of the week based on today, focusing on generic schedule above for now.
+    //starts at the top of this week
+    /* for (let weekdayIndex = 7; weekdayIndex > 0; weekdayIndex--) {
         tableDay = moment().day(0 - weekdayIndex).format("ddd Do");
         $("#table-header").append(
             `<th scope="col">${tableDay}</th>`
-        )
+        );
     }
     //goes into next week
     for (let weekdayIndex = 0; weekdayIndex < 7; weekdayIndex++) {
@@ -54,11 +59,12 @@ $(document).ready(function () {
         $("#table-header").append(
             `<th scope="col">${tableDay}</th>`
         )
-    }
+    } 
+    // *add disable class if todays date is after the date list on the top of the form somehow.....
+    */
 
-    //* need to convert from military time (if statement is attempt of that)
+    //* need to convert from military time
     for (let hourIndex = 7; hourIndex < 22; hourIndex++) {
-        // if (hourIndex < 12) {
         let firstTimeSlotNum = hourIndex;
         let secondTimeSlotNum = hourIndex + 1;
         let timeSlot = `${firstTimeSlotNum}-${secondTimeSlotNum}`
@@ -69,13 +75,6 @@ $(document).ready(function () {
         );
         hourPosition++;
 
-
-        // } else {
-        //     let firstTimeSlotNum = hourIndex - 12;
-        //     let secondTimeSlotNum = hourIndex -12 + 1;
-        //     let timeSlot = `${firstTimeSlotNum}-${secondTimeSlotNum}`
-        //     console.log(timeSlot)
-        // }
     }
 
 
@@ -91,7 +90,7 @@ $(document).ready(function () {
     //-----------------
 
 
-    //-----------------
+    //submit address to Firebase/hide from view
     $("#address-submit-btn").on("click", function (event) {
         event.preventDefault();
         let address = $("#street-address").val().trim();
@@ -105,9 +104,12 @@ $(document).ready(function () {
             state: state,
             zip: zip
         })
-    })
 
-    //this info not terribly helpful yet---left off here!
+        $("#addressForm").addClass("d-none");
+        $("#schedule-form").removeClass("d-none");
+    })
+    
+    //submit schedule to Firebase
     $("#schedule-submit-btn").on("click", function (event) {
         event.preventDefault();
         $(".form-check-input").each(function () {
@@ -119,9 +121,6 @@ $(document).ready(function () {
             availability: userScheduleArray
         })
     })
-
-
-
 
 
 })
