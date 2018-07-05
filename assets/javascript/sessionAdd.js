99252172ec7c9e1f3bfd63d3e0c1545ff230b1b2
richa863 <richardson.brian90@gmail.com>
2018-07-05 11:52:01 -0500

    thursday2

--- assets/javascript/sessionAdd.js
+++ assets/javascript/sessionAdd.js
@@ -64,7 +64,7 @@ $("#sessionSubmit").on("click", function(event){
             $("#sessionZipcode").val("");
             $("#sessionTimeStart").val("");
             $("#sessionTimeEnd").val("");
-            $("#sessionInvite").val("");
+     
     
         
     
